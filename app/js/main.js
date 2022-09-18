9b748ad0b84a011f1629f17812d923757b048c14
IvanChapler <ererer4646@gmail.com>
2022-09-18 20:38:45 +0500

    added interactivity to tabs

--- app/js/main.js
+++ app/js/main.js
@@ -15,10 +15,18 @@ $(document).ready(function(){
             }
         ]
     });
+    //Табы
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
 });
+
 //Эксперимент. Спарсить дата атрибут и вывести его значения json в консоль P.S: Почему-то не робит
 const el = document.querySelector("span");
 let json = el.dataset.profession;
 let data = json.parse(json);
 console.log(data.name);
 console.log(data.profession);
+
